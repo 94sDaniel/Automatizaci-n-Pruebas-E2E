a147c60b5684d0357de5e402a9f3953a73a429da
Daniel <94sdaniel@gmail.com>
2025-12-07 18:59:45 -0500

    Modificacion en serenity.conf y en tarea de navegar al PIM agregando esperas para solcuionar elementos no encontrados al correr pipeline

--- src/test/java/com/orangehrm/automation/tasks/NavigateToPim.java
+++ src/test/java/com/orangehrm/automation/tasks/NavigateToPim.java
@@ -1,13 +1,20 @@
 package com.orangehrm.automation.tasks;
 
-import com.orangehrm.automation.ui.DashboardPage;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.targets.Target;
 import net.serenitybdd.screenplay.waits.WaitUntil;
+import net.serenitybdd.core.Serenity;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static com.orangehrm.automation.ui.DashboardPage.PIM_MENU;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isClickable;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
+import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 public class NavigateToPim implements Task {
 
@@ -17,9 +24,20 @@ public class NavigateToPim implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(
-                WaitUntil.the(DashboardPage.PIM_MENU, isClickable()),
-                Click.on(DashboardPage.PIM_MENU)
-        );
+        try {
+            actor.attemptsTo(
+                    WaitUntil.the(PIM_MENU, isVisible()).forNoMoreThan(10).seconds(),
+                    WaitUntil.the(PIM_MENU, isClickable()).forNoMoreThan(10).seconds(),
+                    Click.on(PIM_MENU)
+            );
+        } catch (Throwable e) {
+            System.out.println("⚠️ Click estándar falló. Intentando 'Force Click' con JS en el menú PIM.");
+
+            WebDriver driver = Serenity.getWebdriverManager().getCurrentDriver();
+            WebElement element = PIM_MENU.resolveFor(actor).getElement();
+
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].click();", element);
+        }
     }
 }
