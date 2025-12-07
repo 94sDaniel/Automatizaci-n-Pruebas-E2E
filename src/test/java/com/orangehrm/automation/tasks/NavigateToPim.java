e0b0855f2cf7500e4f7d84bb2941a970f04aa9ce
Daniel <94sdaniel@gmail.com>
2025-12-07 18:50:25 -0500

    Modificacion en tareas de vavegar al PIM agregando esperas para solcuionar elementos no encontrados al correr pipeline

--- src/test/java/com/orangehrm/automation/tasks/NavigateToPim.java
+++ src/test/java/com/orangehrm/automation/tasks/NavigateToPim.java
@@ -1,13 +1,19 @@
 package com.orangehrm.automation.tasks;
 
 import com.orangehrm.automation.ui.DashboardPage;
+import com.orangehrm.automation.ui.PimPage;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.waits.WaitUntil;
+import net.serenitybdd.core.Serenity;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-import static net.serenitybdd.screenplay.Tasks.instrumented;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isClickable;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
+import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 public class NavigateToPim implements Task {
 
@@ -17,9 +23,17 @@ public class NavigateToPim implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(
-                WaitUntil.the(DashboardPage.PIM_MENU, isClickable()).forNoMoreThan(15).seconds(),
-                Click.on(DashboardPage.PIM_MENU)
-        );
+        try {
+            actor.attemptsTo(
+                    WaitUntil.the(DashboardPage.PIM_MENU, isVisible()).forNoMoreThan(30).seconds(),
+                    WaitUntil.the(DashboardPage.PIM_MENU, isClickable()).forNoMoreThan(30).seconds(),
+                    Click.on(DashboardPage.PIM_MENU)
+            );
+        } catch (Throwable e) {
+            System.out.println("Standard click failed, attempting JS click on PIM menu...");
+            WebDriver driver = Serenity.getWebdriverManager().getCurrentDriver();
+            WebElement element = DashboardPage.PIM_MENU.resolveFor(actor).getElement();
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+        }
     }
 }
